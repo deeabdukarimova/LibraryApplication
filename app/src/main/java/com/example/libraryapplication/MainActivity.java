593ef9819e57b200c7d0546466bdbc5d48bf2373
deeabdukarimova <deeabdukarimova@gmail.com>
2021-02-21 11:46:41 -0600

    Layout file is updated, views are initialized

--- app/src/main/java/com/example/libraryapplication/MainActivity.java
+++ app/src/main/java/com/example/libraryapplication/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.libraryapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //initialize all buttons
+    private Button btnAllBooks,btnFavorites,btnAlreadyRead,btnWantToead,btnCurrentlyReading,btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Initialize Views/Buttons
+        initViews();
+
+    }
+
+    private void initViews() {
+        btnAllBooks=findViewById(R.id.btnallBooks);
+        btnFavorites=findViewById(R.id.btnFavorite);
+        btnAlreadyRead=findViewById(R.id.btnAlreadyRead);
+        btnWantToead=findViewById(R.id.btnWantToRead);
+        btnCurrentlyReading=findViewById(R.id.btnCurrentlyReading);
+        btnAbout=findViewById(R.id.btnAbout);
     }
 }
